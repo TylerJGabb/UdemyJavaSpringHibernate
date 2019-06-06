75149c95414d17bf0e371dc07bb13f0b5081c565
Tyler Gabb <tylerjgabb@gmail.com>
2019-06-05 20:56:38 -0700

    added simple maven project

---
+++ simple-maven-project/simpleapp/src/main/java/com/gabb/simplemaven/simpleapp/App.java
@@ -0,0 +1,13 @@
+package com.gabb.simplemaven.simpleapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World of Maven!!!" );
+    }
+}
